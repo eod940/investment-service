f6f92b0460685d022b129e58120337ed1c441ebe
eod940 <eod940@gmail.com>
2022-09-27 23:29:27 +0900

    :recycle: (feature/integrating) Stock Serializer 작업

--- integration/serializers.py
+++ integration/serializers.py
@@ -1,14 +1,23 @@
 from rest_framework import serializers
 
-from integration.models import Transfer
+from integration.models import Transfer, Stock
 
 
 class TransferSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Transfer
         fields = (
             "transfer_account_number",
             "user_name",
             "transfer_amount"
+        )
+
+
+class StockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stock
+        fields = (
+            "name",
+            "isin",
+            "asset_group"
         )
